1a4242a9629809e8409ac1b8e2ca5d80b3ec71f0
Enzo <enzogvm21@gmail.com>
2020-11-12 19:11:18 -0500

    Creacion de clases

---
+++ src/modelo/Productos.java
@@ -0,0 +1,79 @@
+
+package modelo;
+
+public class Productos {
+    private int id_producto;
+    private String nombre_producto;
+    private double precio;
+    private String fecha_ingreso;
+    private int id_categoria;
+    private int id_proveedores;
+    private int id_detalle;
+    private int id_usuario;
+
+    public int getId_producto() {
+        return id_producto;
+    }
+
+    public void setId_producto(int id_producto) {
+        this.id_producto = id_producto;
+    }
+
+    public String getNombre_producto() {
+        return nombre_producto;
+    }
+
+    public void setNombre_producto(String nombre_producto) {
+        this.nombre_producto = nombre_producto;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getFecha_ingreso() {
+        return fecha_ingreso;
+    }
+
+    public void setFecha_ingreso(String fecha_ingreso) {
+        this.fecha_ingreso = fecha_ingreso;
+    }
+
+    public int getId_categoria() {
+        return id_categoria;
+    }
+
+    public void setId_categoria(int id_categoria) {
+        this.id_categoria = id_categoria;
+    }
+
+    public int getId_proveedores() {
+        return id_proveedores;
+    }
+
+    public void setId_proveedores(int id_proveedores) {
+        this.id_proveedores = id_proveedores;
+    }
+
+    public int getId_detalle() {
+        return id_detalle;
+    }
+
+    public void setId_detalle(int id_detalle) {
+        this.id_detalle = id_detalle;
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+    
+    
+}
---
+++ src/modelo/Proveedores.java
@@ -0,0 +1,24 @@
+
+package modelo;
+
+public class Proveedores {
+    private int id_proveedores;
+    private String nombre_proveedores;
+
+    public int getId_proveedores() {
+        return id_proveedores;
+    }
+
+    public void setId_proveedores(int id_proveedores) {
+        this.id_proveedores = id_proveedores;
+    }
+
+    public String getNombre_proveedores() {
+        return nombre_proveedores;
+    }
+
+    public void setNombre_proveedores(String nombre_proveedores) {
+        this.nombre_proveedores = nombre_proveedores;
+    }
+    
+}
