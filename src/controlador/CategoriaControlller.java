d3829121d7be1f7bc2f98b1b2bd6f59bfc395cad
JosueEMG <josueomega355@gmail.com>
2020-11-19 01:40:29 -0500

    categoria e historial controladores

---
+++ src/controlador/CategoriaControlller.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import modelo.Categoria;
+import util.MySQLConexion;
+
+/**
+ *
+ * @author Josue Emmanuel Medina Garcia
+ */
+public class CategoriaControlller {
+
+    public List<Categoria> listadoCategoria() {
+        List<Categoria> lis = new ArrayList<>();
+        Connection conn = null;
+
+        try {
+            conn = MySQLConexion.getConexion();
+            String sql = "select id_categoria, nombre_categoria from categoria;";
+            PreparedStatement st = conn.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
+            //llenar el arraylist con la clase entidad
+            while (rs.next()) {
+                Categoria c = new Categoria();
+                c.setId_categoria(rs.getInt(1));
+                c.setNombre_Categoria(rs.getString(2));
+                lis.add(c);
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (Exception e2) {
+            }
+        }
+        return lis;
+    }
+    
+    public void addCategoria(String nombre) {
+        Connection conn = null;
+
+        try {
+            conn = MySQLConexion.getConexion();
+            String sql = "insert into categoria (nombre_categoria) values (?);";
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setString(1, nombre);
+            st.executeUpdate();
+            //llenar el arraylist con la clase entidad
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (Exception e2) {
+            }
+        }
+    }
+    
+    public void removeCategoria(String nombre) {
+        Connection conn = null;
+
+        try {
+            conn = MySQLConexion.getConexion();
+            String sql = "delete from categoria where nombre_categoria = ?";
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setString(1, nombre);
+            st.executeUpdate();
+            //llenar el arraylist con la clase entidad
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (Exception e2) {
+            }
+        }
+    }
+}
---
+++ src/controlador/HistorialController.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import modelo.Historial;
+import util.MySQLConexion;
+
+/**
+ *
+ * @author Josue Emmanuel Medina Garcia
+ */
+public class HistorialController {
+
+    public List<Historial> listadoHistorial(String nombre) {
+        List<Historial> lis = new ArrayList<>();
+        Connection conn = null;
+
+        try {
+            if (nombre != null) {
+                conn = MySQLConexion.getConexion();
+                String sql = "select h.id_historial, p.fecha_ingreso, fecha_salida, cantidad, p.nombre_producto\n"
+                        + "from historial h inner join productos p\n"
+                        + "on h.id_producto = p.id_producto and p.nombre_producto LIKE ?;";
+                PreparedStatement st = conn.prepareStatement(sql);
+                st.setString(1, "%" + nombre + "%");
+                ResultSet rs = st.executeQuery();
+                //llenar el arraylist con la clase entidad
+                while (rs.next()) {
+                    Historial h = new Historial();
+                    h.setId_Historial(rs.getInt(1));
+                    h.setFecha_entrada(rs.getString(2));
+                    h.setFecha_salida(rs.getString(3));
+                    h.setCantidad(rs.getInt(4));
+                    h.setNombreProducto(rs.getString(5));
+                    lis.add(h);
+                }
+            } else {
+                conn = MySQLConexion.getConexion();
+                String sql = "select h.id_historial, p.fecha_ingreso, fecha_salida, cantidad, p.nombre_producto\n"
+                        + "from historial h inner join productos p\n"
+                        + "on h.id_producto = p.id_producto";
+                PreparedStatement st = conn.prepareStatement(sql);
+                ResultSet rs = st.executeQuery();
+                //llenar el arraylist con la clase entidad
+                while (rs.next()) {
+                    Historial h = new Historial();
+                    h.setId_Historial(rs.getInt(1));
+                    h.setFecha_entrada(rs.getString(2));
+                    h.setFecha_salida(rs.getString(3));
+                    h.setCantidad(rs.getInt(4));
+                    h.setNombreProducto(rs.getString(5));
+                    lis.add(h);
+                }
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (Exception e2) {
+            }
+        }
+        return lis;
+    }
+}
